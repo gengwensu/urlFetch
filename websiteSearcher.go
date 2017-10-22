f1319b222b7884df56a330d71ad457865ef41d86
Gary Su <gengwensu@yahoo.com>
2017-10-22 15:24:50 -0400

    refactor to block processing

--- websiteSearcher.go
+++ websiteSearcher.go
@@ -32,69 +32,102 @@ type rankURL struct {
 // MAXCONCURRENCY the # of http requests allowed at a time
 var MAXCONCURRENCY = 20
 
+// BLOCKFACTOR multiple of MAXCONCURRENCY to be proccessed each iteration
+var BLOCKFACTOR = 5
+
 var ch = make(chan rankURL)
 var tokens = make(chan struct{}, MAXCONCURRENCY)
 
 var re *regexp.Regexp
 var debug int
+var outFile string
 
 func main() {
 	inFile := flag.String("infile", "urls.txt", "input file for urls")
-	outFile := flag.String("outfile", "out.txt", "output file for urls")
+	oFile := flag.String("outfile", "out.txt", "output file for urls")
 	rePattern := flag.String("regexp", "new.?", "reg exp for matching")
 	debugFlag := flag.Int("debugLevel", 0, "0-off,1-info,2-all")
 	flag.Parse()
 
-	fmt.Printf("Web search running. input %s, output %s, regexp %s\n", *inFile, *outFile, *rePattern)
+	fmt.Printf("Web search running. input=%q, output=%q, regexp=%q\n", *inFile, *oFile, *rePattern)
 	reString := "(?i)" + *rePattern // flag to case insensitive search
 	re = regexp.MustCompile(reString)
 	debug = *debugFlag
+	outFile = *oFile
 
 	urlList := readUrlList(*inFile) // get the urls from file
 	start := time.Now()
-	for _, v := range urlList {
-		go fetch(v.url)
-	}
-
-	for range urlList {
-		output := <-ch
-		key := output.url[7:]
-		switch debug {
-		case 1:
-			fmt.Printf("main: got output from %s\n", key)
-		case 2:
-			fmt.Printf("main: got output from %s, match %s\n", key, output.result)
+	tBlock := BLOCKFACTOR * MAXCONCURRENCY
+	//processing a block of threads at a time to make sure getting some output
+	n := (int)(len(urlList) / tBlock)
+	for i := 0; i <= n; i++ {
+		startIndex := i * tBlock
+		var endIndex int
+		if i == n {
+			endIndex = startIndex + len(urlList)%tBlock
+		} else {
+			endIndex = startIndex + tBlock
 		}
-		output.rank = urlList[key].rank
-		urlList[key] = output
+
+		fmt.Printf("main: processing %d to %d\n", startIndex, endIndex-1)
+		concurrentSearch(urlList[startIndex:endIndex])
 	}
 
-	writeToFile(*outFile, urlList)
 	fmt.Printf("main: %.2fs elapsed overall.\n", time.Since(start).Seconds())
 }
 
-func fetch(url string) {
+func concurrentSearch(urlList []rankURL) {
+	for _, v := range urlList {
+		go fetch(v)
+	}
+
+	var outList []rankURL
+	for range urlList {
+		output := <-ch
+
+		switch debug {
+		case 1:
+			fmt.Printf("concurrentSearch: got output from %s\n", output.url)
+		case 2:
+			fmt.Printf("concurrentSearch: got output from %s, output is %v\n", output.url, output)
+		}
+
+		outList = append(outList, output)
+		if debug > 1 {
+			fmt.Println("concurrentSearch: after append.")
+		}
+	}
+
+	if debug > 1 {
+		fmt.Printf("concurrentSearch: before write to file. outList: %v\n", outList)
+	}
+	writeToFile(outFile, outList)
+}
+
+func fetch(ru rankURL) {
 	start := time.Now()
-	var ru rankURL
-	ru.url = url
+
 	if debug > 0 {
-		fmt.Println(url + " is up " + time.Now().String())
+		fmt.Println(ru.url + " is up " + time.Now().String())
 	}
 	tokens <- struct{}{} // acquire a token
 	if debug > 0 {
-		fmt.Println(url + " got a token " + time.Now().String())
+		fmt.Println(ru.url + " got a token " + time.Now().String())
 	}
 
-	resp, err := http.Get(url)
+	c := &http.Client{ // need to prevent no response case
+		Timeout: 15 * time.Second,
+	}
+	resp, err := c.Get("http://" + ru.url)
 	if err != nil { // can't just exit, need to take care of unblocking others
 		if debug > 0 {
-			fmt.Printf("Get %s, fetch error: %v\n", url, err)
+			fmt.Printf("Get %s, fetch error: %v\n", ru.url, err)
 		}
-		ru.result = fmt.Sprintf("Get %s, fetch error: %v\n", url, err)
+		ru.result = fmt.Sprintf("Get %s, fetch error: %v\n", ru.url, err)
 
 		<-tokens
 		if debug > 0 {
-			fmt.Println(url + " release a token " + time.Now().String())
+			fmt.Println(ru.url + " release a token " + time.Now().String())
 		}
 		ch <- ru
 	} else {
@@ -103,12 +136,12 @@ func fetch(url string) {
 
 		<-tokens // release the token
 		if debug > 0 {
-			fmt.Println(url + " release a token " + time.Now().String())
+			fmt.Println(ru.url + " release a token " + time.Now().String())
 		}
 
 		if err != nil {
-			fmt.Printf("fetch: reading %s error: %v\n", url, err)
-			ru.result = fmt.Sprintf("fetch: reading %s error: %v\n", url, err)
+			fmt.Printf("%s: fetch: reading error: %v\n", ru.url, err)
+			ru.result = fmt.Sprintf("%s: fetch: reading error: %v\n", ru.url, err)
 		} else {
 			src := string(b)
 
@@ -123,18 +156,18 @@ func fetch(url string) {
 
 	secs := time.Since(start).Seconds()
 	if debug > 0 {
-		fmt.Printf("%s: done, %.2fs elapsed\n", url, secs)
+		fmt.Printf("%s: done, %.2fs elapsed\n", ru.url, secs)
 	}
 }
 
-func readUrlList(fName string) map[string]rankURL {
+func readUrlList(fName string) []rankURL {
 	file, err := os.Open(fName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	var result = make(map[string]rankURL)
+	var result []rankURL
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() { // read in the file line by line
@@ -144,36 +177,26 @@ func readUrlList(fName string) map[string]rankURL {
 			s := fieldsOut[1][1 : len(fieldsOut[1])-1] // strip quotes
 			var ru rankURL
 			ru.rank, _ = strconv.Atoi(fieldsOut[0])
-			ru.url = "http://" + s
+			ru.url = s
 			ru.result = ""
-			result[s] = ru
+			result = append(result, ru)
 		}
 	}
 
 	return result
 }
 
-type kv struct { // data structure for sorting the map result
-	key   string
-	value int
-}
+func writeToFile(fName string, rList []rankURL) {
+	sort.Slice(rList, func(i, j int) bool { return rList[i].rank < rList[j].rank })
 
-func writeToFile(fName string, m map[string]rankURL) {
-	var ss []kv
-	for k, v := range m { // need to sort the output by rank
-		ss = append(ss, kv{k, v.rank})
-	}
-	sort.Slice(ss, func(i, j int) bool { return ss[i].value < ss[j].value })
-
-	file, err := os.OpenFile(fName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
+	file, err := os.OpenFile(fName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	for _, e := range ss {
-		ue := m[e.key]
-		fmt.Fprintf(file, "%d, %q, matches found: %s\n", ue.rank, e.key, ue.result)
+	for _, e := range rList {
+		fmt.Fprintf(file, "%d, %q, matches found: %s\n", e.rank, e.url, e.result)
 	}
 
 }
